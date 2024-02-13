9f06344615b70058dc00d5361b63977a88b5c197
bobertsanchezez <henry_earnest@brown.edu>
2024-02-13 15:24:13 -0500

    add ledges to tilemap, add lighting and shadowing

---
+++ scenes/environment/tower.gd
@@ -0,0 +1,13 @@
+extends TileMap
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	#$Lights.rotation = rotate_toward($Lights.rotation, fmod($Lights.rotation + PI / 2, PI), delta * 0.1)
+	#print($Lights.rotation)
+	pass
