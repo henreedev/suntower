22947f140a6bed5be2406468d5bacf4423aa4b18
bobertsanchezez <henry_earnest@brown.edu>
2024-10-14 18:05:36 -0400

    wind beam angle calculations

--- scenes/characters/flower_head.gd
+++ scenes/characters/flower_head.gd
@@ -8,8 +8,8 @@ const GRAVITY = -0.2
 const vine_root_offset := Vector2(0, 5)
 @export var vine_seg : PackedScene
 @export var play_animation_on_start := false
-@export var max_extended_len := 125.0
-const BASE_MAX_EXTENDED_LEN := 125.0
+@export var max_extended_len := 2000.0
+const BASE_MAX_EXTENDED_LEN := 2000.0
 const EXTEND_SPEED = 90.0
 var extend_speed_mod = 1.0
 var extra_len = 0.0
--- scenes/environment/tower.gd
+++ scenes/environment/tower.gd
@@ -25,6 +25,10 @@ var _right_day_start_angle_storm = deg_to_rad(110.0)
 var _right_day_end_angle_storm = deg_to_rad(70.0)
 var _left_day_start_angle_storm = -_right_day_end_angle_storm
 var _left_day_end_angle_storm = -_right_day_start_angle_storm
+var _right_day_start_angle_wind = deg_to_rad(80.0)
+var _right_day_end_angle_wind = deg_to_rad(130.0)
+var _left_day_start_angle_wind = -_right_day_start_angle_wind 
+var _left_day_end_angle_wind = -_right_day_end_angle_wind
 var _goal_rotation = _right_day_start_angle
 var _right = true
 var sunny_modulate := Color(.89, .89, .89, 1.0)
@@ -154,6 +158,25 @@ func _change_weather_on_progress():
 					_goal_rotation = lerp(_left_day_start_angle_storm, _left_day_end_angle_storm, fmod(_day_cycle, _half_day_cycle_dur) / _half_day_cycle_dur)
 		elif weather == Weather.WINDY:
 			_update_wind_strength()
+			
+			const mult = 0.4
+			
+			_day_cycle = fmod(_progress + INITIAL_DAY_OFFSET, _day_cycle_dur * mult)
+			_half_day_cycle_dur = _day_cycle_dur / 2 * mult
+			if 0.0 <= _day_cycle and _day_cycle < _half_day_cycle_dur: 
+				if not _right:
+					var tween := create_tween()
+					tween.tween_property(self, "_goal_rotation", _right_day_start_angle_wind, 0.4).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
+					_right = true
+				else:
+					_goal_rotation = lerp(_right_day_start_angle_wind, _right_day_end_angle_wind, _day_cycle / _half_day_cycle_dur)
+			else:
+				if _right:
+					create_tween().tween_property(self, "_goal_rotation", _left_day_start_angle_wind, 0.4).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
+					_right = false
+				else:
+					_goal_rotation = lerp(_left_day_start_angle_wind, _left_day_end_angle_wind, fmod(_day_cycle, _half_day_cycle_dur) / _half_day_cycle_dur)
+			
 
 func _update_wind_strength():
 	var str = clampf(wind_strength, 1.0, 10.0) # strength of the color of wind particles
@@ -173,7 +196,7 @@ func _act_on_weather_state():
 		Weather.STORMY:
 			pass
 		Weather.WINDY:
-			pass # Wind hitboxes in level activate
+			_lights.set_energy_mult(0.5)
 		Weather.PEACEFUL:
 			pass # Full sun, but hard platforming
 
