29e338229932b4c5bb7006d48c2cd389ad3aea3a
bobertsanchezez <henry_earnest@brown.edu>
2024-10-19 00:07:39 -0400

    new menu

--- scenes/menus/menu.gd
+++ scenes/menus/menu.gd
@@ -2,36 +2,22 @@ extends Control
 
 signal game_started
 var alternate := false
-var alternate2 := false
-var rate = 0.658
-var rate2 = rate * 0.5
+var rate = 0.652 * 2
+
+@onready var title_blur = $Background/TitleBlur
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var tween = create_tween().set_loops()
 	animate_title()
-	tween.tween_callback(animate_title).set_delay(rate)
-	var tween2 = create_tween().set_loops()
-	animate_title2()
-	tween2.tween_callback(animate_title2).set_delay(rate2)
 
 func animate_title():
-	if alternate:
-		create_tween().tween_property($Title, "label_settings:font_color", Color(1.0,  1.0, 0.8), rate)
-		create_tween().tween_property($Title, "rotation", PI / 16, rate ).set_trans(Tween.TRANS_QUAD)
-	else:
-		create_tween().tween_property($Title, "label_settings:font_color", Color(1.5, 1.3, 0.3), rate)
-		create_tween().tween_property($Title, "rotation", -PI / 16, rate ).set_trans(Tween.TRANS_QUAD)
-	alternate = !alternate
-
-
-func animate_title2():
-	if alternate2:
-		create_tween().tween_property($Title, "scale", Vector2(1.2, 1.2), rate2 ).set_trans(Tween.TRANS_LINEAR)
-	else:
-		create_tween().tween_property($Title, "scale", Vector2(1.0, 1.0), rate2 ).set_trans(Tween.TRANS_LINEAR)
-	alternate2 = !alternate2
+	const STR = 1.5
+	var tween = create_tween().set_loops()
+	tween.tween_property(title_blur, "modulate", Color.WHITE * STR, rate).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
+	tween.tween_property(title_blur, "modulate", Color.WHITE, rate).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN)
+	tween.tween_property(title_blur, "modulate", Color.GRAY, rate).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
+	tween.tween_property(title_blur, "modulate", Color.WHITE, rate).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN)
 
 func _on_start_button_pressed():
 	SceneManager.instance.menu_to_game()
