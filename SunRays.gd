90fe9db0da8798cf7cfcf11f2269815f87d68860
bobertsanchezez <henry_earnest@brown.edu>
2024-10-27 18:53:11 -0400

    compatibility fixes (low tickrate, fewer rays)

--- SunRays.gd
+++ SunRays.gd
@@ -4,30 +4,35 @@ class_name SunRays
 
 signal sun_hit
 
-var num_rays = 750
-var offset_height = -num_rays / 2 - 50
+var rays_height = 750
+var ray_ratio = 0.2
+var offset_height = -rays_height / 2 - 50
 var leader = false
 @onready var _player : FlowerHead = get_tree().get_first_node_in_group("flowerhead")
 @onready var _tower : Tower = get_tree().get_first_node_in_group("tower")
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	_make_rays(num_rays)
+	_make_rays(rays_height)
 	get_tree().get_first_node_in_group("sunrays").leader = true
 
 func _make_rays(count):
+	var ray_ratio_counter = 1.0
 	for i in count:
-		var ray = RayCast2D.new()
-		ray.target_position = Vector2(0, 1000)
-		add_child(ray)
-		ray.position = Vector2(0, i)
-		ray.add_to_group("rays")
-		ray.collision_mask = 1 + 2 + 32
-		# Visualization line
-		#var line = Line2D.new()
-		#ray.add_child(line)
-		#line.add_point(Vector2(0, 0))
-		#line.add_point(ray.target_position)
-		#line.width = 1
+		ray_ratio_counter -= ray_ratio
+		if ray_ratio_counter <= 0.0:
+			ray_ratio = 1.0
+			var ray = RayCast2D.new()
+			ray.target_position = Vector2(0, 1000)
+			add_child(ray)
+			ray.position = Vector2(0, i)
+			ray.add_to_group("rays")
+			ray.collision_mask = 1 + 2 + 32
+			# Visualization line
+			var line = Line2D.new()
+			ray.add_child(line)
+			line.add_point(Vector2(0, 0))
+			line.add_point(ray.target_position)
+			line.width = 1
 
 func set_rotate(angle):
 	get_tree().set_group("rays", "rotation", angle)
