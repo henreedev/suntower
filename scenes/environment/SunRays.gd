3d78e1c3bb152a39255a5a8a94a204f492ee48f0
Henry Earnest <henry_earnest@brown.edu>
2025-03-27 20:07:23 -0400

    remove unnecessary raycasts

--- scenes/environment/SunRays.gd
+++ scenes/environment/SunRays.gd
@@ -9,7 +9,7 @@ var offset_height = -num_rays / 2 - 50
 var leader = false
 var vine_tween : Tween
 var curr_rot : float
-var check_stride = 1 # actually check collisions on every n rays (for performance)
+var check_stride = 2 # actually check collisions on every n rays (for performance)
 @export var right = true # are these rays placed on the right side of the tower?
 @onready var _player : Head = get_tree().get_first_node_in_group("flowerhead")
 @onready var _tower : Tower = get_tree().get_first_node_in_group("tower")
@@ -27,7 +27,7 @@ func _make_rays(count):
 		add_child(ray)
 		ray.position = Vector2(0, i)
 		ray.add_to_group("rays")
-		ray.collision_mask = 1 + 2 + 32
+		ray.collision_mask = 1 + 2 + 16 + 32
 		 #Visualization line
 		#if i % 5 == 0:
 			#var line = Line2D.new()
@@ -58,42 +58,47 @@ func _sunrays_pointing_into_tower(sunrays : SunRays):
 	var should_hit = sunrays.right and lights_right || not sunrays.right and not lights_right
 	return should_hit
 
+var i = 0
+
 func _check_player_hit():
-	if _tower.weather == Tower.Weather.STORMY and _tower.lightning_striking:
-		for sunrays : SunRays in get_tree().get_nodes_in_group("sunrays"):
-			if _sunrays_pointing_into_tower(sunrays): # only check rays pointing into tower
-				for ray : RayCast2D in sunrays.get_children():
-					ray.collision_mask = 1 + 2 + 32 # Include vine segs
-					var hit = ray.get_collider()
-					if not hit: continue
-					if hit is Head or hit is Vine or hit.is_in_group("flowerhead"):
-						return true
+	if i == check_stride:
+		i = 1
+		if _tower.weather == Tower.Weather.STORMY and _tower.lightning_striking:
+			for sunrays : SunRays in get_tree().get_nodes_in_group("sunrays"):
+				if _sunrays_pointing_into_tower(sunrays): # only check rays pointing into tower
+					for ray : RayCast2D in sunrays.get_children():
+						ray.collision_mask = 1 + 2 + 16 + 32 # Include vine segs
+						var hit = ray.get_collider()
+						if not hit: continue
+						if hit is Head or hit is Vine or hit.is_in_group("flowerhead"):
+							return true
+			return false
+		elif _tower.weather == Tower.Weather.SUNNY:
+			if not _player._state == Head.State.EXTENDING: 
+				return false
+			for sunrays : SunRays in get_tree().get_nodes_in_group("sunrays"):
+				if _sunrays_pointing_into_tower(sunrays): # only check rays pointing into tower
+					for ray : RayCast2D in sunrays.get_children():
+						var hit = ray.get_collider()
+						if hit is Head or hit is Vine or (hit.is_in_group("flowerhead") if hit else false):
+							#if not _player._animating and hit is Vine:
+								#if not vine_tween: vine_tween = create_tween().set_parallel()
+								#vine_tween.tween_property(hit.sprite, "modulate", Color(1.0, 5.0, 1.0), 0.25)
+								#vine_tween.chain().tween_property(hit.sprite, "modulate", Color(1.0, 1.0, 1.0), 0.25)
+							return true
+			return false
+		elif _tower.weather == Tower.Weather.WINDY:
+			if not _player._state == Head.State.EXTENDING: 
+				return false
+			for sunrays : SunRays in get_tree().get_nodes_in_group("sunrays"):
+				if _sunrays_pointing_into_tower(sunrays): # only check rays pointing into tower
+					for ray : RayCast2D in sunrays.get_children():
+						ray.collision_mask = 1 + 16 + 32 # Exclude vine segs
+						var hit = ray.get_collider()
+						if hit is Head or (hit.is_in_group("flowerhead") if hit else false):
+							return true
+			return false
+		else: return false
+	else:
+		i += 1
 		return false
-	elif _tower.weather == Tower.Weather.SUNNY:
-		var i = 1
-		for sunrays : SunRays in get_tree().get_nodes_in_group("sunrays"):
-			if _sunrays_pointing_into_tower(sunrays): # only check rays pointing into tower
-				for ray : RayCast2D in sunrays.get_children():
-					var hit = ray.get_collider()
-					if i != check_stride: 
-						i = i + 1
-						continue # Skip until stride value is reached
-					else: 
-						i = 1
-					if hit is Head or hit is Vine or (hit.is_in_group("flowerhead") if hit else false):
-						#if not _player._animating and hit is Vine:
-							#if not vine_tween: vine_tween = create_tween().set_parallel()
-							#vine_tween.tween_property(hit.sprite, "modulate", Color(1.0, 5.0, 1.0), 0.25)
-							#vine_tween.chain().tween_property(hit.sprite, "modulate", Color(1.0, 1.0, 1.0), 0.25)
-						return true
-		return false
-	elif _tower.weather == Tower.Weather.WINDY:
-		for sunrays : SunRays in get_tree().get_nodes_in_group("sunrays"):
-			if _sunrays_pointing_into_tower(sunrays): # only check rays pointing into tower
-				for ray : RayCast2D in sunrays.get_children():
-					ray.collision_mask = 1 + 32 # Exclude vine segs
-					var hit = ray.get_collider()
-					if hit is Head or (hit.is_in_group("flowerhead") if hit else false):
-						return true
-		return false
-	else: return false
